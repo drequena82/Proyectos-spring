50f6f05a738a7f986f32335c4118260781e88942
drequena <drequena@everis.com>
2018-09-04 18:02:28 +0200

    Version inicial

---
+++ MiPrimerProyectoSpringBoot/src/main/java/com/example/demo/MiPrimerProyectoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerProyectoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerProyectoSpringBootApplication.class, args);
+	}
+}
