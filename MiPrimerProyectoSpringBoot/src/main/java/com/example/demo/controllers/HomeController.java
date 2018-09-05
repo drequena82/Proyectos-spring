c03dc133902d2522ef0a0d5fa69e3257c07dc9bf
drequena <drequena@everis.com>
2018-09-05 08:02:46 +0200

    04/09/2018

--- MiPrimerProyectoSpringBoot/src/main/java/com/example/demo/controllers/HomeController.java
+++ MiPrimerProyectoSpringBoot/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,13 +1,43 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.Setter;
+import lombok.extern.java.Log;
 
 @Controller
+@Log
+@ConfigurationProperties(prefix="taco.orders")
 public class HomeController {
 	
+	private int limit = 100;
+	
+	@Autowired
+	private MeterRegistry reg; 
+	
 	@GetMapping("/")
-	public String home() {
+	public String home(@RequestParam(value="name",required=false)final String name) {
+		System.out.println("limit: " + limit); 
+		
+		log.info("limit: " + limit);
+		
+		reg.counter("taco." + name.toLowerCase()).increment();
+		
 		return "hola";
 	}
+
+	public int getLimit() {
+		return limit;
+	}
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	
+	
 }
