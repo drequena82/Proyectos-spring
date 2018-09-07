016742c9072611062b54a796ee1aee29ab9b0d8c
drequena <drequena@everis.com>
2018-09-07 20:46:56 +0200

    Restricciones de acceso para el api rest

--- SpringDataAPIrest/src/main/java/io/spring/billing/SpringDataApIrestApplication.java
+++ SpringDataAPIrest/src/main/java/io/spring/billing/SpringDataApIrestApplication.java
@@ -2,11 +2,31 @@ package io.spring.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @SpringBootApplication
 public class SpringDataApIrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApIrestApplication.class, args);
+	}
+
+	
+	//Lo utilizamos para que rest solo haga visibles los repositorios y las funciones con las anotaciones 
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+		return new RepositoryRestConfigurerAdapter() {
+
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+				config.setRepositoryDetectionStrategy(
+						RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
+			}
+		};
 	}
 }
--- SpringDataAPIrest/src/main/java/io/spring/billing/repositories/ProductRepository.java
+++ SpringDataAPIrest/src/main/java/io/spring/billing/repositories/ProductRepository.java
@@ -1,14 +1,21 @@
 package io.spring.billing.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.spring.billing.entities.Product;
 
+@RepositoryRestResource(path="product")
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>{
 
+	@RestResource(path="productGreaterOrEqual")
 	public List<Product> findByPriceGreaterThanEqual(@Param("price")Double price);
 	
+	@RestResource(exported=false)
+	public Optional<Product> findById(Long id);
 }
