4126485101db0ef06ea55f746eb7e5706e2f9edd
drequena <drequena@everis.com>
2018-09-07 20:25:46 +0200

    Incluimos HAL

--- SpringDataAPIrest/src/main/java/io/spring/billing/repositories/ClientRepository.java
+++ SpringDataAPIrest/src/main/java/io/spring/billing/repositories/ClientRepository.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import io.spring.billing.entities.Client;
 
 public interface ClientRepository extends PagingAndSortingRepository<Client, Long>,JpaRepository<Client, Long> {
 
 	
-	public List<Client> findByNameIgnoreCaseAndSurnameIgnoreCase(String name,String surname);
+	public List<Client> findByNameIgnoreCaseAndSurnameIgnoreCase(@Param("name")String name,@Param("surname")String surname);
 	
-	public List<Client> findBySurnameIgnoreCaseOrderByNameDesc(String surname);
+	public List<Client> findBySurnameIgnoreCaseOrderByNameDesc(@Param("surname") String surname);
 }
--- SpringDataAPIrest/src/main/java/io/spring/billing/repositories/ProductRepository.java
+++ SpringDataAPIrest/src/main/java/io/spring/billing/repositories/ProductRepository.java
@@ -3,11 +3,12 @@ package io.spring.billing.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import io.spring.billing.entities.Product;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>{
 
-	public List<Product> findByPriceGreaterThanEqual(Double price);
+	public List<Product> findByPriceGreaterThanEqual(@Param("price")Double price);
 	
 }
